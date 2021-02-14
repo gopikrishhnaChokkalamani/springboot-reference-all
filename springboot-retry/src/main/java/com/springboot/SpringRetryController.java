3b9663ea718e85dc2991e723bfbcac4fb4ae7c74
gopikrishhnachokkalamani <gopikrishhna@gmail.com>
2021-02-13 22:14:24 -0500

    intial

--- springboot-retry/src/main/java/com/springboot/SpringRetryController.java
+++ springboot-retry/src/main/java/com/springboot/SpringRetryController.java
@@ -1,11 +1,14 @@
 package com.springboot;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Recover;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/retry")
 public class SpringRetryController {
@@ -17,12 +20,19 @@ public class SpringRetryController {
   // or else if you are calling a downstream through API gateway, initially before the load
   //picks up, it might result in error, as API gateway might warm up after which requests will be serviced
 
+  //https://dzone.com/articles/spring-retry-way-to-handle-failures
+
+  //try to restrict as much as possible to a specific exception
 
   @GetMapping
-  @Retryable(value = RuntimeException.class)
+  @Retryable(value = RuntimeException.class, maxAttempts = 3, backoff = @Backoff(delay = 5000))
   public String get() {
+    log.info("Exception occurred {}", "get()");
     throw new RuntimeException();
   }
+
+  //if the method has any arguments you can access them in recover method like below
+  //public String recover(RuntimeException e, Student student)
 
   @Recover
   public String recover(RuntimeException e) {
