91b81ec529a63b1c02524aa2d758c4d9a14004dc
gopikrishhnachokkalamani <gopikrishhna@gmail.com>
2021-02-20 18:25:02 -0500

    intial

--- springboot-security/springboot-security-basic-auth/src/main/java/com/springboot/SpringBasicAuthApplication.java
+++ springboot-security/springboot-security-basic-auth/src/main/java/com/springboot/SpringBasicAuthApplication.java
@@ -3,6 +3,7 @@ package com.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -11,6 +12,9 @@ public class SpringBasicAuthApplication {
   //even if u have not added any configurations or any customizations
   //just adding the spring security in the pom.xml will enable the login screen when u access the end point
   public static void main(String[] args) {
+//    BCryptPasswordEncoder e = new BCryptPasswordEncoder();
+//    System.out.println(e.encode("password"));
+
     SpringApplication.run(SpringBasicAuthApplication.class, args);
   }
 }
