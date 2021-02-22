255d97da59994a499a1bac5c0fb549ac3b2e844e
gopikrishhnachokkalamani <gopikrishhna@gmail.com>
2021-02-22 12:07:52 -0500

    intial

--- springboot-actuator/src/main/java/com/springboot/ActuatorController.java
+++ springboot-actuator/src/main/java/com/springboot/ActuatorController.java
@@ -1,16 +1,23 @@
 package com.springboot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/spring/v1")
 public class ActuatorController {
 
   @GetMapping
   public String get() {
+    log.debug("Debug Message");
+    log.trace("Trace Message");
+    log.info("Info Message");
+    log.error("Error Message");
+    log.warn("Warn Message");
     return "Welcome, To actuator demo!";
   }
 }
